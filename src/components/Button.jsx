1dc59476e2a699ce5ec2d6d75d1425946d0cf35b
Kshitij Girkar <girkarkshitij@gmail.com>
2024-12-27 12:21:56 +0530

    feat: button component to render button or link

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+import PropTypes from "prop-types";
+
+function Button({ className, href, onClick, children, px, white }) {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
+
+  if (href) {
+    return (
+      <a href={href} className={classes}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} onClick={onClick}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired,
+  px: PropTypes.string,
+  white: PropTypes.bool,
+};
+
+export default Button;
